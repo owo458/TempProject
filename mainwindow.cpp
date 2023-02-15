d016f0442002a42df43038ef7bbc55180e68a88a
hj_lee <hj.lee@eintelligence.kr>
2023-02-15 14:39:49 +0900

    [내부 파라미터] 첫 커밋

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -22,3 +22,9 @@ void MainWindow::on_pushbtn_extrinsic_clicked()
     this->second_dialog = new ParameterInput(this);
     this->second_dialog->show();
 }
+void MainWindow::on_pushbtn_intrinsic_clicked()
+{
+    this->intrinsic_param_cal_dialog = new intrinsic_param(this);
+    this->intrinsic_param_cal_dialog->show();
+    std::cout<<"show"<<std::endl;
+}
--- mainwindow.h
+++ mainwindow.h
@@ -4,6 +4,7 @@
 #include <QMainWindow>
 
 #include "parameterinput.h"
+#include "intrinsic_param.h"
 
 namespace Ui {
 class MainWindow;
@@ -19,11 +20,13 @@ public:
 
 private slots:
     void on_pushbtn_extrinsic_clicked();
+    void on_pushbtn_intrinsic_clicked();
 
 private:
     Ui::MainWindow *ui;
 
     ParameterInput *second_dialog;
+    intrinsic_param *intrinsic_param_cal_dialog;
 };
 
 #endif // MAINWINDOW_H
